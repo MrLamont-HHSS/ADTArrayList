7ed67ef55c9e432f000ea3a74bb99b97a072d564
Adam Lamont <adam_lamont@wrdsb.ca>
2022-09-27 08:07:43 -0400

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,33 @@
+public class ArrayList{
+       
+   public ArrayList(){
+
+   }
+
+
+   public void add(int element){
+
+   }      
+
+   public void add(int index, int element){
+
+   }
+
+   public int get(int index){
+
+   }
+
+   public int remove(int index){
+
+   }
+
+   public int size(){
+
+   }
+
+   public boolean isEmpty(){
+
+   }
+
+}
+
